30aaa1b7ef5337390a7454f4f27df09127d6e6a7
kylecooltron <kylejcoulon@gmail.com>
2026-03-16 22:39:52 -0600

    temple-assignment

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+const today = new Date();
+document.getElementById("currentyear").innerHTML = `©${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = `Last Modified: ${document.lastModified}`;
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
