0119795feb08ea573b1df43edb0a127e9c6d966b
kylecooltron <kylejcoulon@gmail.com>
2026-04-08 23:27:35 -0600

    product review form

--- scripts/form.js
+++ scripts/form.js
@@ -1,22 +1,40 @@
-// Product array for select dropdown
-document.addEventListener("DOMContentLoaded", function () {
-    const products = [
-        { id: "prod1", name: "Product 1" },
-        { id: "prod2", name: "Product 2" },
-        { id: "prod3", name: "Product 3" },
-    ];
-    const select = document.getElementById("product");
-    // Placeholder option
-    const placeholder = document.createElement("option");
-    placeholder.textContent = "Select a Product ...";
-    placeholder.disabled = true;
-    placeholder.selected = true;
-    select.appendChild(placeholder);
-    // Add products
-    products.forEach((product) => {
-        const option = document.createElement("option");
-        option.value = product.id;
-        option.textContent = product.name;
-        select.appendChild(option);
-    });
+const today = new Date();
+document.getElementById("currentyear").innerHTML = `©${today.getFullYear()}`;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averagerating: 4.5,
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averagerating: 4.7,
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averagerating: 3.5,
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averagerating: 3.9,
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averagerating: 5.0,
+    },
+];
+
+const select = document.getElementById("product");
+
+products.forEach((product) => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    select.appendChild(option);
 });
